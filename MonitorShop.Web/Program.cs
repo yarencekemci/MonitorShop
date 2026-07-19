ddd4210a803a940e1d9e1c6c4196f4004742c4e4
yarencekemci <yarencekemci@gmail.com>
2026-07-19 21:14:42 +0300

    Initial Entity Framework setup completed

--- MonitorShop.Web/Program.cs
+++ MonitorShop.Web/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using MonitorShop.DataAccess.Context;
+
+
 namespace MonitorShop.Web
 {
     public class Program
@@ -8,6 +12,11 @@ namespace MonitorShop.Web
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<MonitorShopDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
 
             var app = builder.Build();
 
