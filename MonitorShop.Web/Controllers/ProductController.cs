25adb2b4e449c5572f3eeed0328f5350b497a767
yarencekemci <yarencekemci@gmail.com>
2026-07-20 23:40:23 +0300

    Improve product category selection

--- MonitorShop.Web/Controllers/ProductController.cs
+++ MonitorShop.Web/Controllers/ProductController.cs
@@ -1,16 +1,21 @@
-﻿using MonitorShop.Entities;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using MonitorShop.Business.Abstract;
+using MonitorShop.Entities;
 
 namespace MonitorShop.Web.Controllers
 {
     public class ProductController : Controller
     {
         private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
 
-        public ProductController(IProductService productService)
+        public ProductController(
+            IProductService productService,
+            ICategoryService categoryService)
         {
             _productService = productService;
+            _categoryService = categoryService;
         }
 
         public IActionResult Index()
@@ -22,23 +27,15 @@ namespace MonitorShop.Web.Controllers
         [HttpGet]
         public IActionResult Create()
         {
+            var categories = _categoryService.GetAll();
+
+            ViewBag.Categories = new SelectList(
+                categories,
+                "Id",
+                "Name"
+            );
+
             return View();
-        }
-
-        [HttpGet]
-        public IActionResult Edit(int id)
-        {
-            var product = _productService.GetById(id);
-            return View(product);
-        }
-
-        public IActionResult Delete(int id)
-        {
-            var product = _productService.GetById(id);
-
-            _productService.Delete(product);
-
-            return RedirectToAction("Index");
         }
 
         [HttpPost]
@@ -48,10 +45,34 @@ namespace MonitorShop.Web.Controllers
             return RedirectToAction("Index");
         }
 
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var product = _productService.GetById(id);
+            var categories = _categoryService.GetAll();
+
+            ViewBag.Categories = new SelectList(
+                categories,
+                "Id",
+                "Name",
+                product.CategoryId
+            );
+
+            return View(product);
+        }
+
         [HttpPost]
         public IActionResult Edit(Product product)
         {
             _productService.Update(product);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var product = _productService.GetById(id);
+            _productService.Delete(product);
+
             return RedirectToAction("Index");
         }
     }
