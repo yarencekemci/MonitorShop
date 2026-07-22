49949a5cf23c05508efb6f8617765e0e8d82ec31
yarencekemci <yarencekemci@gmail.com>
2026-07-23 00:51:12 +0300

    display database products on homepage

--- MonitorShop.Web/Controllers/HomeController.cs
+++ MonitorShop.Web/Controllers/HomeController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MonitorShop.Business.Abstract;
 
 namespace MonitorShop.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IProductService _productService;
+
+        public HomeController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var products = _productService.GetAll();
+
+            return View(products);
         }
     }
 }
