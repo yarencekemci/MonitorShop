85dbe25a724888f1b301cf725a7a0199c3f442d8
yarencekemci <yarencekemci@gmail.com>
2026-07-24 20:09:41 +0300

    Add product details and dynamic categories

--- MonitorShop.Web/Controllers/HomeController.cs
+++ MonitorShop.Web/Controllers/HomeController.cs
@@ -1,20 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MonitorShop.Business.Abstract;
+using MonitorShop.Entities;
 
 namespace MonitorShop.Web.Controllers
 {
     public class HomeController : Controller
     {
         private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
 
-        public HomeController(IProductService productService)
+        public HomeController(
+            IProductService productService,
+            ICategoryService categoryService)
         {
             _productService = productService;
+            _categoryService = categoryService;
         }
 
         public IActionResult Index()
         {
             var products = _productService.GetAll();
+            var categories = _categoryService.GetAll();
+
+            ViewBag.Categories = categories;
 
             return View(products);
         }
