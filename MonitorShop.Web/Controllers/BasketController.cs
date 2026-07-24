8617862443d1a362194dee8942065a62f8b0cf77
yarencekemci <yarencekemci@gmail.com>
2026-07-24 20:48:18 +0300

    complete JavaScript basket operations

---
+++ MonitorShop.Web/Controllers/BasketController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MonitorShop.Web.Controllers
+{
+    public class BasketController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
