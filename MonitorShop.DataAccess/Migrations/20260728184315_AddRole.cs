983a3a4836daeeea655e6e38708a2695da57cb86
yarencekemci <yarencekemci@gmail.com>
2026-07-28 21:51:36 +0300

    Complete role crud operations

---
+++ MonitorShop.DataAccess/Migrations/20260728184315_AddRole.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MonitorShop.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
